7d81a49dcbb9405af80e656e284deae235d0d75f
Pavan Pisupati <mrunalinidivya@gmail.com>
2019-02-21 17:26:08 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear()
+});
+
+
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log = `${now}:${req.method} ${req.url}`;
+  fs.appendFile('server.log',log+'\n',(err)=>{
+    if(err) {
+      console.log('Unalbe to append to server.log');
+    }
+  });
+  console.log(log);
+  res.render('maintainence.hbs');
+    next();
+});
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+})
+
+app.get('/',(req,res)=>{
+res.render('home.hbs',{
+  pageTitle:'Home Page',
+  welcomeMessage:'Welcome!!'
+});
+});
+
+
+
+app.get('/bad',(req,res)=>{
+  res.send({
+    errorMessage:'An error Occured'
+  })
+})
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle:'About Page HBS',
+  });
+});
+
+app.listen(3000,()=>{
+  console.log('server is up and port 3000');
+});
